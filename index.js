a10bea64a4f411a8164f223ec53a41aa8289cc52
mobai <mobai@dian.so>
2019-09-06 17:03:25 +0800

    添加别名支持

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const getFolder = function (sourceFileName) {
     const nametoArray = sourceFileName.split('/')
     return nametoArray.splice(0,nametoArray.length - 1).join('/')
 }
+const getFile = function (sourceFileName, original, aliasSource) {
+    const nametoArray = sourceFileName.split('/')
+    nametoArray.splice(nametoArray.length - 1, 1)
+    if(aliasSource){
+        return original.replace(/([^/]*)/, nPath.resolve(__dirname, '../../', aliasSource))
+    }
+}
 const getSceneSource = function (source, scene, original) {
+    if(!source) return
     const sceneSourceArray = source.match(/([^/]*)$/)[0].split('.')
     sceneSourceArray.splice(1,0,scene)
     const sceneSource = sceneSourceArray.join('.')
@@ -22,25 +30,28 @@ const getSceneSource = function (source, scene, original) {
         return original.replace(/([^/]*)$/, sceneSource)
     }
 }
-
 module.exports = function ({types}) {
     return {
         visitor: {
             ImportDeclaration(path, { opts = {} }){
                 const { node, hub } = path
-                if(!node || !hub) return
-                const scene = process.env.SCENE || opts.scene
-                console.log('babel...', scene)
-                const source = node.source
-                if(scene && fileReg.test(source.value)) {
-                    const specifiers = node.specifiers
+                const { scene = process.env.SCENE, alias, matchAll }  = opts
+                if(!node || !hub || !scene) return
+                const { source, specifiers} = node
+                let sceneSource = null
+                let filename = null
+                let aliasSource = null
+                if(fileReg.test(source.value)){
                     const sourceFolder = getFolder(hub.file.opts.filename)
-                    const filename = nPath.resolve(sourceFolder, source.value)
-                    const sceneSource = getSceneSource(filename, scene, source.value)
-                    if(sceneSource){
-                        const declarations = types.ImportDeclaration(specifiers, types.StringLiteral(sceneSource))
-                        path.replaceWith(declarations)
-                    }
+                    filename = nPath.resolve(sourceFolder, source.value)
+                }else if(alias) {
+                    aliasSource = alias[source.value.match(/([^/]*)/)[0]]
+                    filename = getFile(hub.file.opts.filename, source.value, aliasSource)
+                }
+                sceneSource = getSceneSource(filename, scene, source.value)
+                if(sceneSource || aliasSource){
+                    const declarations = types.ImportDeclaration(specifiers, types.StringLiteral(sceneSource || filename))
+                    path.replaceWith(declarations)
                 }
             }
         }
